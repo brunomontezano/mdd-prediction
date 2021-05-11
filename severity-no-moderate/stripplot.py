8aebce39dc94b03449bbcf815458f89297137e07
Bruno Montezano <brunodoyt@gmail.com>
2021-05-11 10:30:30 -0300

    Add stripplot python script.

--- severity-no-moderate/stripplot.py
+++ severity-no-moderate/stripplot.py
@@ -4,13 +4,13 @@ sns.set_context("paper")
 sns.set_style('whitegrid')
 
 import pandas as pd
-df = pd.read_csv('predictions-depression-severity.csv')
+df = pd.read_csv('ds_plot_prediction.csv')
 print(df)
 
 sns.boxplot(x="Yes", y="outcome", data=df)
 sns.stripplot(x='Yes', y='outcome', color=".3", data=df)
 
-plt.xlabel('Predictions')
-plt.ylabel('Severity')
+plt.xlabel('Predições')
+plt.ylabel('Severidade')
 plt.tight_layout()
 plt.show()
